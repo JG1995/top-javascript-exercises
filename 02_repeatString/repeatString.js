bc26f965a692a8dbc55dd60909942b69b60e6e9e
Jonas Greve <jonas@greve.com>
2024-08-08 14:15:30 +0200

    solve problem 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let returnString = string;
 
+    if (num == 0) {
+        return "";
+    } else if (num < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < num - 1; i++) {
+            returnString += string;
+        }
+        return returnString;
+    }
 };
 
 // Do not edit below this line
