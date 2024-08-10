cc3ef33dbc40cf375d2390a9523b0448819ea9ee
Jonas Greve <jonas@greve.com>
2024-08-10 08:57:58 +0200

    did not solve 12_findTheOldest, had to refer to solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const CurrentDate = new Date().getFullYear();
+
+    function getAge(birth, death) {
+        if (!death) {
+            death = CurrentDate;
+        }
+        return death - birth;
+    }
+
+    people.forEach((person) => {
+        let currentOldest = 0;
+        if (currentOldest > getAge(person.yearOfBirth, person.yearOfDeath)) {
+            currentOldest.name = person.name;
+        }
+        return currentOldest;
+    });
 
 };
 
